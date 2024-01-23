ba7403e2d4a1b85d55c9aeaa31a0a4d979171e23
Pavel Belokon <pbelokon@myseneca.ca>
2024-01-23 17:06:17 -0500

    added express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import http from "http";
+import app from "./server.js";
 
-const server = http.createServer((req, res) => {
-  if ((req.method === "GET") & (req.url === "/")) {
-    res.end();
-  }
-});
-
-server.listen(3001, () => {
-  console.log("server on http://localhost:3001");
+app.listen(3001, () => {
+  console.log("hello on http://localhost:3001");
 });
